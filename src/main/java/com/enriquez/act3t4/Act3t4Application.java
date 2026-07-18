b9a20fdf394d68401ec92e149a5f9730f796fc84
AlejandroGuillermo7 <ale1111prepa7@gmail.com>
2026-07-18 14:18:36 -0600

    Lest go

---
+++ src/main/java/com/enriquez/act3t4/Act3t4Application.java
@@ -0,0 +1,13 @@
+package com.enriquez.act3t4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Act3t4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Act3t4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/enriquez/act3t4/Act3t4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enriquez.act3t4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Act3t4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
